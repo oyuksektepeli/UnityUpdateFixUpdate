515c2e53779e8b3b0eacccbc5679b20ce75e6089
Onur <onuryuksektepeli@hotmail.com.tr>
2020-09-05 00:36:34 +0300

    Initial commit

---
+++ Assets/Scripts/UpdateFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // Used for reqular updates such as:
+    // Moving non-physics objects
+    // Simple Timers
+    // Receiving Input
+
+    // Updates Interval times vary
+    void Update()
+    {
+        Debug.Log("Update time: " + Time.deltaTime);
+    }
+
+    // Called every physics step
+    // FixedUpdate intervals are consistent
+    // Adjusting physics (RigidBody) objects
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate time: " + Time.deltaTime);
+    }
+
+
+}
